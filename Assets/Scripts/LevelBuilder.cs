ba9d8353d6fc5d40d14eabc1ddaa24ef7ede9773
you e <eoghanjtoreilly1@gmail.com>
2024-09-24 18:59:53 +0100

    New test room + trying to get positions fixed
    - This is doing me 'ead in sir.

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
@@ -6,6 +7,7 @@ using Random = UnityEngine.Random;
 
 public class LevelBuilder : MonoBehaviour
 {
+    public static LevelBuilder Instance { get; private set; }
     public enum LevelMode
     {
         TEST,
@@ -15,17 +17,46 @@ public class LevelBuilder : MonoBehaviour
     }
 
     public LevelMode currentFloor;
-    public List<GameObject> possibleRooms;
+    private GameObject _startingRoom;
+    private GameObject _roomToSpawnOn; //The room containing the wall this room used as its spawn position.
+    public List<GameObject> possibleRooms; //Rooms that CAN spawn.
+    public List<GameObject> spawnedRooms; //Rooms that have ALREADY spawned.
     public List<Vector3> spawnPoints; //TODO: Make this unnecessary
     public int numberOfRooms;
-    
+    public Transform spawnWallL, spawnWallR, spawnWallT, spawnWallB;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("More than one LevelBuilder script in the scene.");
+        }
+        Instance = this;
+    }
+
     void Start()
     {
+        _startingRoom = GameObject.FindWithTag("StartingRoom");
+        GetStartingRoomWalls();
         possibleRooms = new List<GameObject>();
         AddRoomsToList();
         SpawnRooms();
     }
 
+    void GetStartingRoomWalls()
+    {
+        spawnWallL = _startingRoom.GetComponent<RoomInfo>().wallL;
+        spawnWallR = _startingRoom.GetComponent<RoomInfo>().wallR;
+        spawnWallB = _startingRoom.GetComponent<RoomInfo>().wallB;
+        spawnWallT = _startingRoom.GetComponent<RoomInfo>().wallT;
+        spawnPoints = new List<Vector3>()
+        {
+            spawnWallL.transform.position,
+            spawnWallR.transform.position,
+            spawnWallB.transform.position,
+            spawnWallT.transform.position
+        };
+    }
     void AddRoomsToList()
     {
         string path = "YOU SHOULDN'T SEE THIS"; //Path for floor exclusive rooms
@@ -54,17 +85,47 @@ public class LevelBuilder : MonoBehaviour
 
     void SpawnRooms()
     {
-        int randomNumber;
-        object roomToSpawn;
+        int randomNumber, randomNumber2;
+        GameObject roomToSpawn;
         for (int i = 0; i < numberOfRooms; i++)
-        {
+        { 
             randomNumber = Random.Range(0, possibleRooms.Count);
-            //TEST CODE, DO NOT USE
-            roomToSpawn = Instantiate(possibleRooms[randomNumber], spawnPoints[i], Quaternion.identity);
-            Debug.Log("Spawned " + possibleRooms[randomNumber] +" at " + spawnPoints[i]);
-            possibleRooms.Remove(possibleRooms[randomNumber]);
+            randomNumber2 = Random.Range(0, spawnPoints.Count);
+            Vector3 spawnPoint = spawnPoints[randomNumber2];
+            switch (possibleRooms[randomNumber].gameObject.name)
+            {      
+                case "LeftWall":
+                    spawnPoint.x -= possibleRooms[randomNumber].GetComponent<RoomInfo>().distToRoomCentre.x;
+                    break; 
+                case "RightWall":
+                    spawnPoint.x += possibleRooms[randomNumber].GetComponent<RoomInfo>().distToRoomCentre.x;
+                    break; 
+                case "BottomWall":
+                    spawnPoint.y -= possibleRooms[randomNumber].GetComponent<RoomInfo>().distToRoomCentre.y;
+                    break; 
+                case "TopWall":
+                    spawnPoint.y += possibleRooms[randomNumber].GetComponent<RoomInfo>().distToRoomCentre.y;
+                   break;
+            }
+            roomToSpawn = Instantiate(possibleRooms[randomNumber], spawnPoint, Quaternion.identity);
+            Debug.Log("Spawned " + possibleRooms[randomNumber] + " at " + spawnPoint);
+            UpdateSpawnWalls(roomToSpawn, randomNumber, randomNumber2);
+          
         }
     }
+
+    void UpdateSpawnWalls(GameObject spawnedRoom, int randomNumber, int randomNumber2)
+    {
+        RoomInfo roomInfo = spawnedRoom.GetComponent<RoomInfo>();
+        spawnPoints.Add(roomInfo.wallT.transform.position);
+        spawnPoints.Add(roomInfo.wallB.transform.position);
+        spawnPoints.Add(roomInfo.wallL.transform.position);
+        spawnPoints.Add(roomInfo.wallR.transform.position);
+        spawnedRooms.Add(possibleRooms[randomNumber]);
+        possibleRooms.Remove(possibleRooms[randomNumber]);
+        spawnPoints.Remove(spawnPoints[randomNumber2]);
+    }
+    
     void Update()
     {
         
---
+++ Assets/Scripts/RoomInfo.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomInfo : MonoBehaviour
+{
+    public Transform wallL, wallR, wallB, wallT;
+    public bool hasLeftRoom, hasRightRoom, hasTopRoom, hasBottomRoom;
+    public Vector3 distToRoomCentre;
+    public List<GameObject> allWalls;
+    void Awake()
+    {
+        foreach (var walls in gameObject.GetComponentsInChildren<Transform>())
+        {
+            if (walls.name.Contains("Wall"))
+            {
+                allWalls.Add(walls.gameObject);
+            }
+        }
+
+        foreach (var walls in allWalls)
+        {
+            switch (walls.name)
+            {
+                case "LeftWall":
+                    wallL = walls.transform;
+                    break;
+                case "RightWall":
+                    wallR = walls.transform;
+                    break;
+                case "TopWall":
+                    wallT = walls.transform;
+                    break;
+                case "BottomWall":
+                    wallB = walls.transform;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        distToRoomCentre.x = (wallL.transform.localPosition.x - wallR.transform.localPosition.x)/2;
+        Debug.Log(gameObject + "Distance between left/right walls and centre: " + distToRoomCentre.x);
+        distToRoomCentre.y = (wallT.transform.localPosition.y - wallB.transform.localPosition.y)/2;
+        Debug.Log(gameObject + "Distance between top/bottom walls and centre: " + distToRoomCentre.y);
+    }
+    
+    void Update()
+    {
+        
+    }
+}
