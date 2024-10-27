91cfb992d21787f91b67b5f3d22870ceedee5924
erikwtn <99894612+erikwtn@users.noreply.github.com>
2024-10-27 17:02:44 +0000

    pause menu works with controller now

--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -1,20 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class MenuHandler : MonoBehaviour
 {
 	[SerializeField] private GameObject inventoryGui;
 	[SerializeField] private GameObject menu;
-	
+	[SerializeField] private GameObject selectedMenu;
+	[SerializeField] private EventSystem eventSystem;
+
 	public void ToggleInventory()
 	{
+		Debug.Log("Toggle Inventory");
 		inventoryGui.SetActive(!inventoryGui.activeSelf);
 	}
 
 	public void Pause(InputAction.CallbackContext context)
 	{
 		menu.SetActive(!menu.activeSelf);
+		SwitchSelected(selectedMenu);
+	}
+
+
+	private void SwitchSelected(GameObject g)
+	{
+		eventSystem.SetSelectedGameObject(null);
+		eventSystem.SetSelectedGameObject(g);
 	}
 }
