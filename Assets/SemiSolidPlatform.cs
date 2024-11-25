5d5d58b3dd63e5102fc82af34d29fe5739699eb1
you e <eoghanjtoreilly1@gmail.com>
2024-11-25 14:28:04 +0000

    Prefab push

--- Assets/SemiSolidPlatform.cs
+++ Assets/SemiSolidPlatform.cs
@@ -6,22 +6,47 @@ using UnityEngine;
 public class SemiSolidPlatform : MonoBehaviour
 {
     private BoxCollider _boxCollider;
+    public string _layerToIgnore;
+    public LayerMask interactableObjects;
     public LayerMask layersToExclude, layersToInclude;
     private void Awake()
     {
         _boxCollider = GetComponent<BoxCollider>();
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            
+            _layerToIgnore = "Player";
         }
+        else if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            _layerToIgnore = "Enemy";
+        }
+        TurnOffCollision(_layerToIgnore);
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            _layerToIgnore = "Player";
+        }
+        else if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            _layerToIgnore = "Enemy";
+        }
+        TurnOnCollision(_layerToIgnore);
     }
 
-    public void TurnOffCollision()
+    public void TurnOffCollision(string layerMask)
     {
-        //_boxCollider.excludeLayers(layersToExclude);
+        _boxCollider.excludeLayers += LayerMask.NameToLayer(layerMask);
+    }
+
+    public void TurnOnCollision(string layerMask)
+    {
+        _boxCollider.excludeLayers -= LayerMask.NameToLayer(layerMask);
     }
 }
