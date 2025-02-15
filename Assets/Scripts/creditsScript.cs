5dec03ce3e6d8d347d2496fb2e1b843f0115675c
Mia-Finn <92232844+Mia-Finn@users.noreply.github.com>
2025-02-15 23:12:03 +0000

    credits working

---
+++ Assets/Scripts/creditsScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class creditsScript : MonoBehaviour
+{
+    public bool isDone;
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (gameObject.)
+        {
+            isDone = true;
+        }
+        else
+        {
+            isDone = false;
+        }
+        */
+    }
+}
