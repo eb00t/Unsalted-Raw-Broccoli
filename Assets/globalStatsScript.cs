e69a346552fe38866f687cd57d96b7e6348143fb
Mia-Finn <92232844+Mia-Finn@users.noreply.github.com>
2025-02-18 21:22:59 +0000

    Death Counter

--- Assets/globalStatsScript.cs
+++ Assets/globalStatsScript.cs
@@ -7,10 +7,18 @@ public class globalStatsScript : MonoBehaviour
 {
     public float deathCounter;
     public TextMeshProUGUI totalDeaths;
-  //  public GameObject deathScreen;
+   // public GameObject deathScreen;
+
+    void OnEnable()
+    {
+        oneDeath();
+        Debug.Log("AWAKE CALLED");
+    }
 
     void Start()
     {
+        loadData();
+
         if (PlayerPrefs.GetFloat("DeathCounter") != null)
         {
             deathCounter = PlayerPrefs.GetFloat("DeathCounter");
@@ -20,51 +28,66 @@ public class globalStatsScript : MonoBehaviour
     }
 
     void Update()
-    {
-      //  saveData();
-    }
-
-    public void saveData()
-    {
-        PlayerPrefs.SetFloat("DeathCounter", deathCounter);
-    }
-
-    public void loadData()
-    {
-        PlayerPrefs.GetFloat("DeathCounter");
-    }
+        {
+            //  saveData();
+        }
 
     public void oneDeath()
     {
-        deathCounter++;
+        //Adds a death to the counter
+
+        deathCounter += 1;
         totalDeaths.text = "Total Deaths = " + deathCounter;
     }
 
-    public void deleteData()
-    {
-        PlayerPrefs.DeleteKey("DeathCounter");
-    }
-
-    /*
-    // Start is called before the first frame update
-    void Start()
-    {
-       if(PlayerPrefs.GetFloat("DeathCounter") != null)
+    public void saveData()
         {
-            deathCounter = PlayerPrefs.GetFloat("DeathCounter");
-        }
-
-        totalDeaths.text = "Total Deaths = " + deathCounter;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(deathScreen.activeInHierarchy == true)
-        {
-            deathCounter++;
+            //Saves the current number that is dispalyed
             PlayerPrefs.SetFloat("DeathCounter", deathCounter);
         }
+
+    public void loadData()
+        {
+            //Loads the counter value that is saved
+            PlayerPrefs.GetFloat("DeathCounter");
+        }
+
+    public void deleteData()
+        {
+            PlayerPrefs.DeleteKey("DeathCounter");
+        }
+
     }
-    */
-}
+
+
+
+
+
+
+
+
+
+
+
+/*
+   // Start is called before the first frame update
+   void Start()
+   {
+      if(PlayerPrefs.GetFloat("DeathCounter") != null)
+       {
+           deathCounter = PlayerPrefs.GetFloat("DeathCounter");
+       }
+
+       totalDeaths.text = "Total Deaths = " + deathCounter;
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+       if(deathScreen.activeInHierarchy == true)
+       {
+           deathCounter++;
+           PlayerPrefs.SetFloat("DeathCounter", deathCounter);
+       }
+   }
+   */
