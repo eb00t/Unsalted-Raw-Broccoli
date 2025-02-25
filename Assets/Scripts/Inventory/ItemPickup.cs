431a801be0a360933e8835f536af6e6e3459f9b8
erikwtn <99894612+erikwtn@users.noreply.github.com>
2025-02-25 16:45:37 +0000

    pick up item prompt behaves better with multiple items, items now have outlines when player can pick them up

--- Assets/Scripts/Inventory/ItemPickup.cs
+++ Assets/Scripts/Inventory/ItemPickup.cs
@@ -10,69 +10,29 @@ public class ItemPickup : MonoBehaviour
     public bool canPickup;
     private CharacterMovement _characterMovement;
     private InventoryStore _inventoryStore;
-    private Consumable _consumable;
-    private RectTransform _rectTransform;
+    [SerializeField] private GameObject outLineSprite;
 
     private void Start()
     {
         _player = GameObject.FindGameObjectWithTag("Player").transform;
         _characterMovement = _player.GetComponent<CharacterMovement>();
         _inventoryStore = GameObject.FindGameObjectWithTag("UIManager").GetComponent<InventoryStore>();
-        _prompt = GameObject.FindGameObjectWithTag("Prompt").gameObject;
-        _text = _prompt.GetComponentInChildren<TextMeshProUGUI>();
-        _consumable = GetComponent<Consumable>();
-        _rectTransform = _prompt.GetComponent<RectTransform>();
     }
-
+    
     private void Update()
     {
         if (_characterMovement.uiOpen || !gameObject.activeSelf) return;
         
         var dist = Vector3.Distance(_player.position, transform.position);
-        
-        if (dist <= range)
-        {
-            //_prompt.SetActive(true);
-            _rectTransform.anchoredPosition = new Vector3(0, 200, 0);
-            _text.text = "Pick up " + _consumable.title + "[O] / Backspace";
-            canPickup = true;
-        }
-        else
-        {
-            //_prompt.SetActive(false);
-            _rectTransform.anchoredPosition = new Vector3(0, -200, 0);
-            _text.text = "";
-            canPickup = false;
-        }
+        canPickup = dist <= range;
+        outLineSprite.SetActive(canPickup);
     }
-
     public void AddItemToInventory()
     {
         _inventoryStore.AddNewItem(gameObject);
-        _rectTransform.anchoredPosition = new Vector3(0, -200, 0);
-        //_prompt.SetActive(false);
         gameObject.SetActive(false);
         canPickup = false;
     }
-
-    /*
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log(other.name + " : " + other.gameObject.name);
-        if (other.CompareTag("Player"))
-        {
-            _animator.SetTrigger("SlideIn");
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            _animator.SetTrigger("SlideOut");
-        }
-    }
-    */
 
     private void OnDrawGizmosSelected()
     {
--- Assets/Scripts/Inventory/ItemPickupHandler.cs
+++ Assets/Scripts/Inventory/ItemPickupHandler.cs
@@ -1,27 +1,62 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class ItemPickupHandler : MonoBehaviour
 {
     private Transform _player;
     private CharacterMovement _characterMovement;
-
+    private GameObject _prompt;
+    private RectTransform _rectTransform;
+    private TextMeshProUGUI _text;
+    
     private void Start()
     {
         _player = GameObject.FindGameObjectWithTag("Player").transform;
         _characterMovement = _player.GetComponent<CharacterMovement>();
+        _prompt = GameObject.FindGameObjectWithTag("Prompt");
+        _rectTransform = _prompt.GetComponent<RectTransform>();
+        _text = _prompt.GetComponentInChildren<TextMeshProUGUI>();
     }
-    public void PickUpItem(InputAction.CallbackContext context)
+    
+    private void Update()
     {
-        if (context.performed)
+        if (_characterMovement.uiOpen) return;
+        
+        var itemCount = 0;
+        
+        foreach (var item in GameObject.FindGameObjectsWithTag("Item"))
         {
-            if (_characterMovement.uiOpen) return;
-            CheckItems();
+            var ip = item.GetComponent<ItemPickup>();
+
+            if (!ip.canPickup) continue;
+            itemCount++;
+        }
+        
+        switch (itemCount)
+        {
+            case 0:
+                _rectTransform.anchoredPosition = new Vector3(0, -200, 0);
+                _text.text = "";
+                break;
+            case 1:
+                _rectTransform.anchoredPosition = new Vector3(0, 200, 0);
+                _text.text = "Pick up item [O] / Backspace";
+                break;
+            case > 1:
+                _rectTransform.anchoredPosition = new Vector3(0, 200, 0);
+                _text.text = "Pick up items [O] / Backspace";
+                break;
         }
     }
 
-    private void CheckItems()
+    
+    public void PickUpItem(InputAction.CallbackContext context)
     {
+        if (!context.performed) return;
+        if (_characterMovement.uiOpen) return;
+        
         foreach (var item in GameObject.FindGameObjectsWithTag("Item"))
         {
             var ip = item.GetComponent<ItemPickup>();
