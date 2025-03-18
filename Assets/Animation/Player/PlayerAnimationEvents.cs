69b92832f11dd4e64ea02beb9c866fbcc582dfc6
erikwtn <99894612+erikwtn@users.noreply.github.com>
2025-03-18 13:24:19 +0000

    changes to player movement, wall jumping, added dash, increased gravity

--- Assets/Animation/Player/PlayerAnimationEvents.cs
+++ Assets/Animation/Player/PlayerAnimationEvents.cs
@@ -48,5 +48,13 @@ public class PlayerAnimationEvents : MonoBehaviour
         charAttack.animEnd = true;
     }
 
-   
+    public void setIFrameOn()
+    {
+        charAttack.isInvulnerable = true;
+    }
+    
+    public void setIFrameOff()
+    {
+        charAttack.isInvulnerable = false;
+    }
 }
