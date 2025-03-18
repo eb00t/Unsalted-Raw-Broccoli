0ce5e8374956e21f09c531bd4df41ce977a904e6
Mia-Finn <92232844+Mia-Finn@users.noreply.github.com>
2025-03-18 15:48:46 +0000

    end scene fix

--- Assets/Scripts/UI/Dialogue/dialogueControllerScript.cs
+++ Assets/Scripts/UI/Dialogue/dialogueControllerScript.cs
@@ -5,151 +5,7 @@ using TMPro;
 
 public class dialogueControllerScript : MonoBehaviour
 {
-    /*
-     * 
-    public TextMeshProUGUI DialogueText;
-    public string[] Sentences;
-    private int Index = 0;
-    public float DialogueSpeed;
-    public float fasterSpeed;
-    public GameObject dialogueCanvas, yesText, noText, normalText; // normalBox;
-
-    //Bools
-    public bool canText = false;
-   // public bool YesOrNo = false; //true = yes, false = no
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        NextSentence();
-       // Debug.Log("Sentence started");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //Move to next sentence
-        if(Input.GetKeyDown(KeyCode.Tab) && dialogueCanvas.activeInHierarchy == true && canText == true)
-        {
-            NextSentence();
-            DialogueSpeed = 0.05f;
-           // Debug.Log("Next sentence started");
-        }
-
-        /*
-        //Skip Dialogue
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            SkipDialogue();
-        }
-        */
-
-    /*
-
-        dialogueFinished();
-
-        //Speed up dialogue
-        if (Input.GetKeyDown(KeyCode.LeftControl) && dialogueCanvas.activeInHierarchy == true)
-        {
-            speedUptext();
-        }
-
-        
-        //Qusetion answers
-        if(Input.GetKeyDown(KeyCode.Y)) //YesOrNo == true &&
-        {
-            //Answer is yes
-            answerY();
-           // normalText.SetActive(false);
-            //normalBox.SetActive(false);
-        }
-        else if (Input.GetKeyDown(KeyCode.N)) //YesOrNo == false && 
-        {
-            //Answer is no
-            answerN();
-          //  normalText.SetActive(false);
-        }
-        else if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            noText.SetActive(false);
-            yesText.SetActive(false);
-        }
-        
-        //dialogueFinished();
-    }
-
-    void NextSentence()
-    {
-        if(Index <= Sentences.Length - 1)
-        {
-            DialogueText.text = "";
-            StartCoroutine(WriteSentence());
-
-            canText = false;
-        }
-    }
-
-    void speedUptext()
-    {
-        DialogueSpeed = fasterSpeed;
-    }
-
-*/
-
-    /*
-    void SkipDialogue()
-    {
-        //Turns off canvas
-        dialogueCanvas.SetActive(false);
-
-       //Debug.Log("Text Skipped!");
-    }
-    */
-
-
-    /*
-    void dialogueFinished()
-    {
-        if (Index == Sentences.Length )
-        {
-            dialogueCanvas.SetActive(false);
-         //   Debug.Log("Text Done!");
-        }
-    }
-
-    void answerY()
-    {
-        yesText.SetActive(true);
-        noText.SetActive(false);
-        normalText.SetActive(false);
-        Debug.Log("YES!");
-    }
-
-    void answerN()
-    {
-        noText.SetActive(true);
-        yesText.SetActive(false);
-        normalText.SetActive(false);
-        Debug.Log("NO");
-    }
-
-    IEnumerator WriteSentence()
-    {
-        foreach(char Character in Sentences[Index].ToCharArray())
-        {
-            DialogueText.text += Character;
-            yield return new WaitForSeconds(DialogueSpeed);
-        }
-
-        Index++;
-        canText = true;
-    }
-
-    */
-
-
-
-    //TESTING ANOTHER WAY TO DO DIALOGUE
+    //DIALOGUE CODE
     public TextMeshProUGUI DialogueText;
     public string[] Sentences;
     private int Index; // = 0;
@@ -160,12 +16,14 @@ public class dialogueControllerScript : MonoBehaviour
 
     private void Start()
     {
+        //Start writing sentences
         DialogueText.text = string.Empty;
         startSentence();
     }
 
     private void Update()
     {
+        //Move through sentences
        if(Input.GetKeyDown(KeyCode.Tab))
         {
             if(DialogueText.text == Sentences[Index])
@@ -244,3 +102,163 @@ public class dialogueControllerScript : MonoBehaviour
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * 
+public TextMeshProUGUI DialogueText;
+public string[] Sentences;
+private int Index = 0;
+public float DialogueSpeed;
+public float fasterSpeed;
+public GameObject dialogueCanvas, yesText, noText, normalText; // normalBox;
+
+//Bools
+public bool canText = false;
+// public bool YesOrNo = false; //true = yes, false = no
+
+// Start is called before the first frame update
+void Start()
+{
+    NextSentence();
+   // Debug.Log("Sentence started");
+}
+
+// Update is called once per frame
+void Update()
+{
+    //Move to next sentence
+    if(Input.GetKeyDown(KeyCode.Tab) && dialogueCanvas.activeInHierarchy == true && canText == true)
+    {
+        NextSentence();
+        DialogueSpeed = 0.05f;
+       // Debug.Log("Next sentence started");
+    }
+
+    /*
+    //Skip Dialogue
+    if (Input.GetKeyDown(KeyCode.Tab))
+    {
+        SkipDialogue();
+    }
+    */
+
+/*
+
+    dialogueFinished();
+
+    //Speed up dialogue
+    if (Input.GetKeyDown(KeyCode.LeftControl) && dialogueCanvas.activeInHierarchy == true)
+    {
+        speedUptext();
+    }
+
+
+    //Qusetion answers
+    if(Input.GetKeyDown(KeyCode.Y)) //YesOrNo == true &&
+    {
+        //Answer is yes
+        answerY();
+       // normalText.SetActive(false);
+        //normalBox.SetActive(false);
+    }
+    else if (Input.GetKeyDown(KeyCode.N)) //YesOrNo == false && 
+    {
+        //Answer is no
+        answerN();
+      //  normalText.SetActive(false);
+    }
+    else if(Input.GetKeyDown(KeyCode.Escape))
+    {
+        noText.SetActive(false);
+        yesText.SetActive(false);
+    }
+
+    //dialogueFinished();
+}
+
+void NextSentence()
+{
+    if(Index <= Sentences.Length - 1)
+    {
+        DialogueText.text = "";
+        StartCoroutine(WriteSentence());
+
+        canText = false;
+    }
+}
+
+void speedUptext()
+{
+    DialogueSpeed = fasterSpeed;
+}
+
+*/
+
+/*
+void SkipDialogue()
+{
+    //Turns off canvas
+    dialogueCanvas.SetActive(false);
+
+   //Debug.Log("Text Skipped!");
+}
+*/
+
+
+/*
+void dialogueFinished()
+{
+    if (Index == Sentences.Length )
+    {
+        dialogueCanvas.SetActive(false);
+     //   Debug.Log("Text Done!");
+    }
+}
+
+void answerY()
+{
+    yesText.SetActive(true);
+    noText.SetActive(false);
+    normalText.SetActive(false);
+    Debug.Log("YES!");
+}
+
+void answerN()
+{
+    noText.SetActive(true);
+    yesText.SetActive(false);
+    normalText.SetActive(false);
+    Debug.Log("NO");
+}
+
+IEnumerator WriteSentence()
+{
+    foreach(char Character in Sentences[Index].ToCharArray())
+    {
+        DialogueText.text += Character;
+        yield return new WaitForSeconds(DialogueSpeed);
+    }
+
+    Index++;
+    canText = true;
+}
+
+*/
+
+
