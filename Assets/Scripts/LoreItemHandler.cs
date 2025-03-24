d01640f16af94bea795b56fcba428d04c53f32f0
you e <eoghanjtoreilly1@gmail.com>
2025-03-24 17:51:25 +0000

    Forgot to add lore spawns to other lore rooms

--- Assets/Scripts/LoreItemHandler.cs
+++ Assets/Scripts/LoreItemHandler.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class LoreItemHandler : ScriptableObject
 {
     public string loreTitle;
+    public string whoWroteThis;
     [TextArea(3, 10)]
     public string[] loreBodyText;
     public bool discoveredByPlayer;
