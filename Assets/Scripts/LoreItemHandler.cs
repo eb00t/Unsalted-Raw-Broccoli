25863638d3dce1089ed23d3349ad2207c0684009
you e <eoghanjtoreilly1@gmail.com>
2025-03-24 17:56:56 +0000

    Update LoreItemHandler.cs

--- Assets/Scripts/LoreItemHandler.cs
+++ Assets/Scripts/LoreItemHandler.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Lore Item", menuName = "ScriptableObjects/Lore Item Handler", order = 2)]
 public class LoreItemHandler : ScriptableObject
 {
+    [field: Tooltip("Whatever you want to call your lore document.")]
     public string loreTitle;
-    public string whoWroteThis;
+    [field: Tooltip("If your document is written from the perspective of an unknown person, put ???? here.")]
+    public string whoWroteThis; 
+    [field: Tooltip("The contents of the lore document.")]
     [TextArea(3, 10)]
     public string[] loreBodyText;
+    [field: Tooltip("If this is set to true, the lore will not spawn in levels, only within the lore room of the intermission.")]
     public bool discoveredByPlayer;
+    
 
 }
