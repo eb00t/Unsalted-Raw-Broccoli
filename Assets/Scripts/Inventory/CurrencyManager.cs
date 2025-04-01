1650db866cf7216a049e6485f078a02f4b8d758d
erikwtn <99894612+erikwtn@users.noreply.github.com>
2025-04-01 12:36:26 +0100

    added animation and count coroutine for when currency is changed

--- Assets/Scripts/Inventory/CurrencyManager.cs
+++ Assets/Scripts/Inventory/CurrencyManager.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -6,23 +6,54 @@ public class CurrencyManager : MonoBehaviour
 {
     [SerializeField] private DataHolder dataHolder;
     [SerializeField] private TextMeshProUGUI currencyHeldText;
+    [SerializeField] private float countDuration = 0.5f;
+    [SerializeField] private GameObject newCurrencyObj;
+    private Animator _animator;
+    private TextMeshProUGUI _newCurrencyTxt;
+    private Coroutine _currencyCounting;
 
     private void Start()
     {
         currencyHeldText.text = dataHolder.currencyHeld.ToString();
+        _animator = newCurrencyObj.GetComponent<Animator>();
+        _newCurrencyTxt = newCurrencyObj.GetComponent<TextMeshProUGUI>();
     }
 
     public void UpdateCurrency(int amount)
     {
-        if (dataHolder.currencyHeld + amount <= 0)
+        var oldValue = dataHolder.currencyHeld;
+        dataHolder.currencyHeld = Mathf.Max(dataHolder.currencyHeld + amount, 0);
+        var newValue = dataHolder.currencyHeld;
+
+        var changeAmount = newValue - oldValue;
+
+        if (changeAmount != 0)
         {
-            dataHolder.currencyHeld = 0;
+            _newCurrencyTxt.text = (changeAmount > 0 ? "+ " : "- ") + Mathf.Abs(changeAmount);
+            _animator.SetTrigger("currencyChanged");
         }
-        else
+
+        if (_currencyCounting != null)
         {
-            dataHolder.currencyHeld += amount;   
+            StopCoroutine(_currencyCounting);
         }
-        
-        currencyHeldText.text = dataHolder.currencyHeld.ToString();
+
+        _currencyCounting = StartCoroutine(UpdateOverTime(oldValue, newValue));
+    }
+
+    private IEnumerator UpdateOverTime(int startValue, int endValue)
+    {
+        var elapsed = 0f;
+
+        while (elapsed < countDuration)
+        {
+            elapsed += Time.deltaTime;
+            var t = Mathf.Clamp01(elapsed / countDuration);
+            var currentDisplay = Mathf.RoundToInt(Mathf.Lerp(startValue, endValue, t));
+            currencyHeldText.text = currentDisplay.ToString();
+            yield return null;
+        }
+
+        currencyHeldText.text = endValue.ToString();
     }
 }
