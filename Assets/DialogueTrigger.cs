389d9a982c9f851482163cd0d6fef2d21400fed4
you e <eoghanjtoreilly1@gmail.com>
2025-04-09 13:26:31 +0100

    Logo + minor cam fix

--- Assets/DialogueTrigger.cs
+++ Assets/DialogueTrigger.cs
@@ -24,6 +24,11 @@ public class DialogueTrigger : MonoBehaviour
         {
             gameObject.SetActive(false);
         }
+
+        if (dataHolder.currentLevel is not (LevelBuilder.LevelMode.Floor1 or LevelBuilder.LevelMode.Intermission))
+        {
+            gameObject.SetActive(false);
+        }
     }
 
     private void OnTriggerStay(Collider other)
