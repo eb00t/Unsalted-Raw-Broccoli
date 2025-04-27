38ea17f8f238fd47f8fb5f4022ab2f79d94b77a5
erikwtn <99894612+erikwtn@users.noreply.github.com>
2025-04-27 12:04:07 +0100

    added small delay for npc flips to make it feel more natural

--- Assets/FacePlayer.cs
+++ Assets/FacePlayer.cs
@@ -7,8 +7,11 @@ public class FacePlayer : MonoBehaviour
 {
     [SerializeField] private bool isFacingRight;
     [SerializeField] private float facePlayerDistance;
+    [SerializeField] private float flipDelay;
     private Transform _player;
     private Vector3 _originalScale;
+    private bool _canFlip;
+    private Coroutine _flipCoroutine;
 
     private void Awake()
     {
@@ -21,19 +24,36 @@ public class FacePlayer : MonoBehaviour
         var playerDir = transform.position - _player.position;
         var playerDist = Vector3.Distance(transform.position, _player.position);
 
-        if (!(playerDist <= facePlayerDistance))
+        if (playerDist > facePlayerDistance && _canFlip)
         {
             transform.localScale = _originalScale;
             return;
         }
         
-        if (playerDir.x < 0)
+        if (playerDir.x < 0 && _canFlip)
         {
             transform.localScale = !isFacingRight ? new Vector3(-_originalScale.x, _originalScale.y, _originalScale.z) : _originalScale;
+            StartCoroutine(DelayFlip());
         }
-        else if (playerDir.x >= 0)
+        else if (playerDir.x >= 0 && _canFlip)
         {
             transform.localScale = !isFacingRight ? _originalScale : new Vector3(-_originalScale.x, _originalScale.y, _originalScale.z);
+            StartCoroutine(DelayFlip());
         }
+        else if (!_canFlip)
+        {
+            if (_flipCoroutine == null)
+            {
+                _flipCoroutine = StartCoroutine(DelayFlip());
+            }
+        }
+    }
+
+    private IEnumerator DelayFlip()
+    {
+        _canFlip = false;
+        yield return new WaitForSeconds(flipDelay);
+        _flipCoroutine = null;
+        _canFlip = true;
     }
 }
