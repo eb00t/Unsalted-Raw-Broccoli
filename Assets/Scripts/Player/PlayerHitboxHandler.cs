0567fe238e70b40ac177216ca6511e403ddfcda1
erikwtn <99894612+erikwtn@users.noreply.github.com>
2025-05-05 13:40:15 +0100

    player slash vfx implemented

--- Assets/Scripts/Player/PlayerHitboxHandler.cs
+++ Assets/Scripts/Player/PlayerHitboxHandler.cs
@@ -16,7 +16,15 @@ public class PlayerHitboxHandler : MonoBehaviour
                 var sign = Mathf.Sign(GetComponentInParent<CharacterMovement>().transform.localScale.x);
                 var rot = sign == 1 ? 90f : -90f;
                 var vfx = Instantiate(impactVFX, impactOrigin.position, Quaternion.Euler(0f, 0f, rot));
-                vfx.transform.localScale = new Vector3(Mathf.Abs(vfx.transform.localScale.x) * sign, vfx.transform.localScale.y, vfx.transform.localScale.z);
+                
+                if (characterAttack.gameObject.layer == 13)
+                {
+                    vfx.transform.localScale = new Vector3(Mathf.Abs(vfx.transform.localScale.x) * sign * .5f, vfx.transform.localScale.y * .5f, vfx.transform.localScale.z);
+                }
+                else if (characterAttack.gameObject.layer == 14)
+                {
+                    vfx.transform.localScale = new Vector3(Mathf.Abs(vfx.transform.localScale.x) * sign * 2f, vfx.transform.localScale.y * 2f, vfx.transform.localScale.z);
+                }
             }
         }
     }
