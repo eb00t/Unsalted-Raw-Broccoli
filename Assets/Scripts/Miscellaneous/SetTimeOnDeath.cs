6ced1142c95e73b5bd3806ae64590f6bec756312
erikwtn <99894612+erikwtn@users.noreply.github.com>
2025-06-22 16:31:55 +0100

    death screen redesign

--- Assets/Scripts/Miscellaneous/SetTimeOnDeath.cs
+++ Assets/Scripts/Miscellaneous/SetTimeOnDeath.cs
@@ -1,11 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using DG.Tweening;
 
 public class SetTimeOnDeath : MonoBehaviour
 {
+    private static readonly int StaticLaugh = Animator.StringToHash("StaticLaugh");
+    [SerializeField] private CanvasGroup hudCanvasGroup;
+    [SerializeField] private Animator animator;
+
     public void SetTimeScale()
     {
         Time.timeScale = 0f;
+    }
+
+    public void FadeHudOut()
+    {
+        hudCanvasGroup.DOFade(0f, 0.5f);
+    }
+
+    public void StartLaugh()
+    {
+        animator.SetBool(StaticLaugh, true);
     }
 }
